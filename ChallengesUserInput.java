534a911e2de3fa5fac9ac41b89d4bd5cdc071a91
Yogita <yogitamaruvapalli@gmail.com>
2025-12-16 14:39:09 +0000

    challenges

--- ChallengesUserInput.java
+++ ChallengesUserInput.java
@@ -5,24 +5,36 @@ public class ChallengesUserInput {
 
       //Challenge 1
 
-      System.out.println("Enter one word: ");
-      String one = input.nextLine();
-      System.out.println("Enter another word: ");
-      String two = input.nextLine();
+      //System.out.println("Enter one word: ");
+      //String one = input.nextLine();
+      //System.out.println("Enter another word: ");
+      //String two = input.nextLine();
 
-      System.out.println("Enter a number:");
-      String num1 = input.nextLine();
-      int num1Converted  = Integer.parseInt(num1);
+      //System.out.println("Enter a number:");
+      //String num1 = input.nextLine();
+      //int num1Converted  = Integer.parseInt(num1);
 
-      System.out.println("Enter another number:");
-      String num2 = input.nextLine();
-      int num2Converted  = Integer.parseInt(num2);
+      //System.out.println("Enter another number:");
+      //String num2 = input.nextLine();
+      //int num2Converted  = Integer.parseInt(num2);
 
-      if ((num1Converted + num2Converted) < 100) {
-        System.out.println(one + two + (num1Converted + num2Converted) + "@gmail.com");
-    
-    
+      //if ((num1Converted + num2Converted) < 100) {
+        //System.out.println(one + two + (num1Converted + num2Converted) + "@gmail.com");
+      //}
+
+      String password = "Hellothisisthepassword";
+
+      System.out.println("Enter the password: ");
+      String newpassword = input.nextLine();
+
+      if (password.equals(newpassword)) {
+        System.out.println("Access granted");
+      }else{
+        System.out.println("Access denied");
       }
+
+     System.out.println("Enter a number: ");
+     int num1 = input.nextLine();
 
     }
 }

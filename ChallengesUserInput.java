89829f098eaf07c5490444d4b9f4c68e310f9d76
Yogita <yogitamaruvapalli@gmail.com>
2025-12-16 21:48:02 +0000

    updating challenges

--- ChallengesUserInput.java
+++ ChallengesUserInput.java
@@ -22,6 +22,8 @@ public class ChallengesUserInput {
         //System.out.println(one + two + (num1Converted + num2Converted) + "@gmail.com");
       //}
 
+      //Challenge 1: Conditionals
+
       String password = "Hellothisisthepassword";
 
       System.out.println("Enter the password: ");
@@ -33,8 +35,34 @@ public class ChallengesUserInput {
         System.out.println("Access denied");
       }
 
+      //Challenge 2: Conditionals
+
      System.out.println("Enter a number: ");
-     int num1 = input.nextLine();
+     int num1 = input.nextInt();
+
+     if ((num1)%2 != 0) {
+      System.out.println("The number you entered is odd.");
+     } else {
+      System.out.println("The number you entered is even.");
+     }
+
+     //Challenge 3: Conditionals
+     System.out.println("Enter a number grade: ");
+     int grade = input.nextInt();
+
+     if (grade>= 90) {
+      System.out.println("Grade: A");
+     } else if (grade>=80 && grade<90){
+      System.out.println("Grade: B");
+     } else if (grade >= 70  && grade <80) {
+      System.out.println("Grade: C");
+     } else if (grade>= 60 && grade < 70) {
+      System.out.println("Grade: D");
+     } else {
+      System.out.println("You failed :(");
+     }
+
+
 
     }
 }

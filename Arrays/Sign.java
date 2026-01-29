7633178d2ed0017bb4cd2457f29edfd2fbc8d3b8
Yogita <yogitamaruvapalli@gmail.com>
2026-01-29 02:26:26 +0000

    FRQ Sign Finished Product

---
+++ Arrays/Sign.java
@@ -0,0 +1,42 @@
+public class Sign {
+    private String message;
+    private int width;
+
+    public Sign (String message, int width) {
+        this.message = message;
+        this.width = width;
+    }
+
+    public void numberOfLines() {
+        double len = message.length();
+        if (len < width) {
+            System.out.println("1");
+        } else {
+            double lines = len/width;
+            System.out.println(Math.round(lines));
+        }
+    }
+
+    public void getLines(){
+        if (width == message.length() || width > message.length()) {
+           System.out.println(message); 
+        } else {
+        for (int i =0; i <= message.length(); i+=width) {
+           if ((i+width) < message.length()) {
+            String extracted = message.substring(i,i+width); 
+           System.out.print(extracted + ";");
+           } }
+        System.out.println();
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Sign sign2 = new Sign("ABCDE", 2);
+
+        sign2.getLines();
+        sign2.numberOfLines();
+
+
+    }
+}
